fab702d28f79cb8c5956892268c20990ce017402
Samuel Malec <xmalec@fi.muni.cz>
2023-10-07 11:49:36 +0200

    create helper classes for TableModel

--- src/main/java/cz/fi/muni/pv168/easyfood/ui/model/Column.java
+++ src/main/java/cz/fi/muni/pv168/easyfood/ui/model/Column.java
@@ -1,6 +1,53 @@
-package cz.fi.muni.pv168.easyfood.ui.model;/**
- * 
- * @author Samuel Malec
- */
-public class Column {
+package cz.fi.muni.pv168.easyfood.ui.model;
+
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+class Column<E, T> {
+
+    private final String name;
+    private final Function<E, T> valueGetter;
+    private final BiConsumer<E, T> valueSetter;
+    private final Class<T> columnType;
+
+    private Column(String name, Class<T> columnClass, Function<E, T> valueGetter, BiConsumer<E, T> valueSetter) {
+        this.name = Objects.requireNonNull(name, "name cannot be null");
+        this.columnType = Objects.requireNonNull(columnClass, "column class cannot be null");
+        this.valueGetter = Objects.requireNonNull(valueGetter, "value getter cannot be null");
+        this.valueSetter = valueSetter;
+    }
+
+    public static <E, T> Column<E, T> editable(String name, Class<T> columnClass, Function<E, T> valueGetter,
+                                               BiConsumer<E, T> valueSetter) {
+        return new Column<>(name, columnClass, valueGetter,
+                Objects.requireNonNull(valueSetter, "value setter cannot be null"));
+    }
+
+    public static <E, T> Column<E, T> readonly(String name, Class<T> columnClass, Function<E, T> valueGetter) {
+        return new Column<>(name, columnClass, valueGetter, null);
+    }
+
+    void setValue(Object value, E entity) {
+        if (valueSetter == null) {
+            throw new UnsupportedOperationException("Cannot set value in readonly column: '" + name + "'");
+        }
+        valueSetter.accept(entity, columnType.cast(value));
+    }
+
+    T getValue(E entity) {
+        return valueGetter.apply(entity);
+    }
+
+    String getName() {
+        return name;
+    }
+
+    Class<?> getColumnType() {
+        return columnType;
+    }
+
+    boolean isEditable() {
+        return valueSetter != null;
+    }
 }
