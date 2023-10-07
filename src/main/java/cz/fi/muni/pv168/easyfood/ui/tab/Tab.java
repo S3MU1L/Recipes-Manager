9a9207fcfa7ee158cd63bcbc3048593158867861
Samuel Malec <xmalec@fi.muni.cz>
2023-10-07 22:36:58 +0200

    change constructor of Tab and fix TabContainer

--- src/main/java/cz/fi/muni/pv168/easyfood/ui/tab/Tab.java
+++ src/main/java/cz/fi/muni/pv168/easyfood/ui/tab/Tab.java
@@ -1,15 +1,20 @@
 package cz.fi.muni.pv168.easyfood.ui.tab;
 
-import javax.swing.*;
+import cz.fi.muni.pv168.easyfood.ui.tablemodel.EntityTableModel;
 
-public class Tab<E> {
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class Tab<E extends EntityTableModel> {
     private JComponent component;
     private String title;
     private JTable table;
+    private final E model;
 
-    public Tab(String title, JTable table) {
+    public Tab(String title, JTable table, E model) {
         this.title = title;
         this.table = table;
+        this.model = model;
         this.component = new JScrollPane(table);
     }
 
@@ -35,6 +40,18 @@ public class Tab<E> {
 
     public void setTable(JTable table) {
         this.table = table;
+    }
+
+    public E getModel() {
+        return model;
+    }
+
+    public void delete() {
+        int rows[] = table.getSelectedRows();
+        for (int i = rows.length - 1; i >= 0; i--) {
+            int modelRow = table.convertRowIndexToModel(rows[i]);
+            model.deleteRow(modelRow);
+        }
     }
 
 }
