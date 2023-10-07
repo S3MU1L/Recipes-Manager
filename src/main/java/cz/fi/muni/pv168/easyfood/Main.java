64127a5eefa4b49b1702b0d3b5258af35746397c
Samuel Malec <xmalec@fi.muni.cz>
2023-10-07 11:45:00 +0200

    implement Main class

--- src/main/java/cz/fi/muni/pv168/easyfood/Main.java
+++ src/main/java/cz/fi/muni/pv168/easyfood/Main.java
@@ -1,71 +1,29 @@
-package cz.fi.muni.pv168.dietaryAssistantApp;
+package cz.fi.muni.pv168.easyfood;
 
-import cz.fi.muni.pv168.dietaryAssistantApp.data.IngredientDao;
-import cz.fi.muni.pv168.dietaryAssistantApp.data.RecipeDao;
-import cz.fi.muni.pv168.dietaryAssistantApp.data.TestDataGenerator;
-import cz.fi.muni.pv168.dietaryAssistantApp.service.IngredientService;
-import cz.fi.muni.pv168.dietaryAssistantApp.service.RecipeService;
-import cz.fi.muni.pv168.dietaryAssistantApp.ui.windows.MainWindow;
-import cz.fi.muni.pv168.dietaryAssistantApp.ui.windows.WindowsManager;
 
-import javax.sql.DataSource;
-import javax.swing.*;
-import java.awt.*;
+import cz.fi.muni.pv168.easyfood.ui.MainWindow;
+
+import javax.swing.UIManager;
+import java.awt.EventQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-
-    private static boolean debug = false;
-
-    public static boolean isDebug() {
-        return debug;
-    }
-
     public static void main(String[] args) {
-        for (String arg : args) {
-            if (arg.equals("DEBUG")) {
-                debug = true;
-                break;
-            }
-        }
-        TestDataGenerator testDataGenerator = new TestDataGenerator();
-        var dataSource = createDatasource();
-        IngredientDao ingredientDao = new IngredientDao(dataSource, testDataGenerator);
-        RecipeDao recipeDao = new RecipeDao(dataSource, ingredientDao, testDataGenerator);
         initNimbusLookAndFeel();
-        var windowsManager = new WindowsManager();
-        var recipeService = new RecipeService(recipeDao, windowsManager);
-        var ingredientService = new IngredientService(ingredientDao, windowsManager, recipeService);
-        windowsManager.setIngredientService(ingredientService);
-        windowsManager.setRecipeService(recipeService);
-        EventQueue.invokeLater(() -> windowsManager.openWindow(
-                new MainWindow(recipeService, ingredientService)));
-    }
-
-    private static DataSource createDatasource() {
-        EmbeddedDataSource dataSource = new EmbeddedDataSource();
-        String dbPath = System.getProperty("user.home") + "/dietary-assistant-pv168";
-        dataSource.setDatabaseName(dbPath);
-        dataSource.setCreateDatabase("create");
-        return dataSource;
+        EventQueue.invokeLater(() -> new MainWindow().show());
     }
 
     private static void initNimbusLookAndFeel() {
         try {
             for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
         } catch (Exception ex) {
-            Logger.getLogger(Main.class.getName()).log(Level.INFO, "Nimbus layout initialization failed", ex);
-            try {
-                UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-            } catch (Exception ex2) {
-                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "CrossPlatform layout initialization failed", ex2);
-            }
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "Nimbus layout initialization failed", ex);
         }
     }
 }
