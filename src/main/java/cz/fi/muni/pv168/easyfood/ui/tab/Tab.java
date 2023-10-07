fee651f8b91db711b5f28d4136b2437384e15955
Samuel Malec <xmalec@fi.muni.cz>
2023-10-07 21:03:19 +0200

    implement TabContainer and Tab Class

--- src/main/java/cz/fi/muni/pv168/easyfood/ui/tab/Tab.java
+++ src/main/java/cz/fi/muni/pv168/easyfood/ui/tab/Tab.java
@@ -1,6 +1,40 @@
-package cz.fi.muni.pv168.easyfood.ui.tab;/**
- * 
- * @author Samuel Malec
- */
-public class Tab {
+package cz.fi.muni.pv168.easyfood.ui.tab;
+
+import javax.swing.*;
+
+public class Tab<E> {
+    private JComponent component;
+    private String title;
+    private JTable table;
+
+    public Tab(String title, JTable table) {
+        this.title = title;
+        this.table = table;
+        this.component = new JScrollPane(table);
+    }
+
+    public JComponent getComponent() {
+        return component;
+    }
+
+    public void setComponent(JComponent component) {
+        this.component = component;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public JTable getTable() {
+        return table;
+    }
+
+    public void setTable(JTable table) {
+        this.table = table;
+    }
+
 }
--- src/main/java/cz/fi/muni/pv168/easyfood/ui/tab/TabContainer.java
+++ src/main/java/cz/fi/muni/pv168/easyfood/ui/tab/TabContainer.java
@@ -1,6 +1,39 @@
-package cz.fi.muni.pv168.easyfood.ui.tab;/**
- * 
- * @author Samuel Malec
- */
+package cz.fi.muni.pv168.easyfood.ui.tab;
+
+import javax.swing.*;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.util.IdentityHashMap;
+import java.util.Map;
+
 public class TabContainer {
+
+    private final JTabbedPane tabbedPane = new JTabbedPane();
+    private final Map<Component, Tab<?>> tabs = new IdentityHashMap<>();
+
+    public void addChangeListener(ChangeListener l) {
+        tabbedPane.addChangeListener(l);
+    }
+
+    public JComponent getComponent() {
+        return tabbedPane;
+    }
+
+    public void addTab(Tab<?> tab) {
+        JComponent component = tab.getComponent();
+        tabbedPane.addTab(tab.getTitle(), component);
+        tabs.put(component, tab);
+    }
+
+    public Tab<?> getSelectedTab() {
+        Component selectedComponent = tabbedPane.getSelectedComponent();
+        if (selectedComponent == null) {
+            throw new IllegalStateException("No tab is selected");
+        }
+        Tab<?> tab = tabs.get(selectedComponent);
+        if (tab == null) {
+            throw new AssertionError("Unknown tabbedPane selectedComponent");
+        }
+        return tab;
+    }
 }
