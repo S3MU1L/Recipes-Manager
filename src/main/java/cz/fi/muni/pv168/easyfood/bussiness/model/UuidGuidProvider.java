d019bdf29da2489a8bd155011481919637461694
Samuel Malec <xmalec@fi.muni.cz>
2023-11-18 23:44:19 +0100

    Create class UiudGuidProvider

--- src/main/java/cz/fi/muni/pv168/easyfood/bussiness/model/UuidGuidProvider.java
+++ src/main/java/cz/fi/muni/pv168/easyfood/bussiness/model/UuidGuidProvider.java
@@ -1,6 +1,11 @@
-package cz.fi.muni.pv168.easyfood.bussiness.model;/**
- * 
- * @author Samuel Malec
- */
-public class UuidGuidProvider {
+package cz.fi.muni.pv168.easyfood.bussiness.model;
+
+import java.util.UUID;
+
+public class UuidGuidProvider implements GuidProvider {
+
+    @Override
+    public String newGuid() {
+        return UUID.randomUUID().toString();
+    }
 }
