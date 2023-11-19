459ea0ea9b9b887edc7fbb1ada2c4cf341bf7f88
Samuel Malec <xmalec@fi.muni.cz>
2023-11-19 03:01:15 +0100

    create DataStorageException

--- src/main/java/cz/fi/muni/pv168/easyfood/storage/DataStorageException.java
+++ src/main/java/cz/fi/muni/pv168/easyfood/storage/DataStorageException.java
@@ -1,6 +1,18 @@
-package cz.fi.muni.pv168.easyfood.storage;/**
- * 
- * @author Samuel Malec
+package cz.fi.muni.pv168.easyfood.storage;
+
+/**
+ * Exception that is thrown if there is some problem with data storage
  */
-public class DataStorageException {
+public class DataStorageException extends RuntimeException {
+
+    private static final long serialVersionUID = 0L;
+
+    public DataStorageException(String message) {
+        this(message, null);
+    }
+
+    public DataStorageException(String message, Throwable cause) {
+        super("Storage error: " + message, cause);
+    }
 }
+
