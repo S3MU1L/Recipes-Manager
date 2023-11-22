24983f05bf58bc531b388caef747ce6c93883fba
xmalec <xmalec@fi.muni.cz>
2023-11-22 15:18:29 +0100

    delete unused/deprecated classes

--- src/main/java/cz/fi/muni/pv168/easyfood/bussiness/service/export/batch/Batch.java
+++
@@ -1,9 +0,0 @@
-package cz.fi.muni.pv168.easyfood.bussiness.service.export.batch;
-
-import cz.fi.muni.pv168.easyfood.bussiness.model.Ingredient;
-import cz.fi.muni.pv168.easyfood.bussiness.model.Recipe;
-
-import java.util.Collection;
-
-public record Batch(Collection<Recipe> recipes, Collection<Ingredient> ingredients) {
-}
