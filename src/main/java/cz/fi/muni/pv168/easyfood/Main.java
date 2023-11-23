547da3ad8b6c7e97923c3843f9b1a47251dd4463
xmalec <xmalec@fi.muni.cz>
2023-11-23 22:04:10 +0100

    refactor Main.java

--- src/main/java/cz/fi/muni/pv168/easyfood/Main.java
+++ src/main/java/cz/fi/muni/pv168/easyfood/Main.java
@@ -1,17 +1,33 @@
 package cz.fi.muni.pv168.easyfood;
 
 
+import cz.fi.muni.pv168.easyfood.ui.ApplicationErrorHandler;
 import cz.fi.muni.pv168.easyfood.ui.MainWindow;
+import cz.fi.muni.pv168.easyfood.ui.action.NuclearQuitAction;
+import cz.fi.muni.pv168.easyfood.ui.action.QuitAction;
 
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import java.awt.EventQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
+
     public static void main(String[] args) {
+        var errorHandler = new ApplicationErrorHandler();
+        Thread.setDefaultUncaughtExceptionHandler(errorHandler);
+
         initNimbusLookAndFeel();
-        EventQueue.invokeLater(() -> new MainWindow().show());
+
+        EventQueue.invokeLater(() -> {
+            try {
+                new MainWindow().show();
+            } catch (Exception ex) {
+                showInitializationFailedDialog(ex);
+            }
+        });
     }
 
     private static void initNimbusLookAndFeel() {
@@ -25,5 +41,25 @@ public class Main {
         } catch (Exception ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "Nimbus layout initialization failed", ex);
         }
+    }
+
+    private static void showInitializationFailedDialog(Exception ex) {
+        EventQueue.invokeLater(() -> {
+            ex.printStackTrace();
+            Object[] options = {
+                    new JButton(new QuitAction()),
+                    new JButton(new NuclearQuitAction())
+            };
+            JOptionPane.showOptionDialog(
+                    null,
+                    "Application initialization failed.\nWhat do you want to do?",
+                    "Initialization Error",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.ERROR_MESSAGE,
+                    null,
+                    options,
+                    options[0]
+            );
+        });
     }
 }
