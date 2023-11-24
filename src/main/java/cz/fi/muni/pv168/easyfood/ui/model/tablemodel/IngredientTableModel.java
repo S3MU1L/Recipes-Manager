9d08887516ba539105ea554bdf88e51687f5e544
Samuel Sabo <xsabo1@fi.muni.cz>
2023-11-24 19:40:23 +0100

    Ingredients works on CrudService

--- src/main/java/cz/fi/muni/pv168/easyfood/ui/model/tablemodel/IngredientTableModel.java
+++ src/main/java/cz/fi/muni/pv168/easyfood/ui/model/tablemodel/IngredientTableModel.java
@@ -7,9 +7,7 @@ import cz.fi.muni.pv168.easyfood.bussiness.service.crud.CrudService;
 import cz.fi.muni.pv168.easyfood.ui.model.Column;
 
 import javax.swing.JOptionPane;
-import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
-import java.awt.Component;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,10 +20,9 @@ public class IngredientTableModel extends AbstractTableModel implements EntityTa
     private final CrudService<Ingredient> ingredientCrudService;
     private final CrudService<Recipe> recipeCrudService;
 
-    private final List<Column<Ingredient, ?>> columns = List.of(
-            Column.readonly("Name", String.class, Ingredient::getName),
-            Column.readonly("Calories", String.class, Ingredient::getFormattedCalories)
-    );
+    private final List<Column<Ingredient, ?>> columns =
+            List.of(Column.readonly("Name", String.class, Ingredient::getName),
+                    Column.readonly("Calories", String.class, Ingredient::getFormattedCalories));
 
     public IngredientTableModel(CrudService<Ingredient> ingredientCrudService, CrudService<Recipe> recipeCrudService) {
         this.ingredientCrudService = ingredientCrudService;
@@ -68,24 +65,16 @@ public class IngredientTableModel extends AbstractTableModel implements EntityTa
     public void addRow(Ingredient ingredient) {
         int newRowIndex = ingredients.size();
         ingredients.add(ingredient);
+        ingredientCrudService.create(ingredient);
         fireTableRowsInserted(newRowIndex, newRowIndex);
     }
 
     public void updateRow(Ingredient oldIngredient, Ingredient newIngredient) {
+        ingredientCrudService.deleteByGuid(oldIngredient.getGuid());
+        ingredientCrudService.create(newIngredient);
         int rowIndex = ingredients.indexOf(oldIngredient);
         ingredients.set(rowIndex, newIngredient);
         fireTableRowsUpdated(rowIndex, rowIndex);
-    }
-
-
-    @Override
-    public void customizeTableCell(Component cell, Object value, int row, JTable table) {
-
-    }
-
-    @Override
-    public void customizeTable(JTable table) {
-
     }
 
     public Ingredient getEntity(int rowIndex) {
@@ -100,15 +89,16 @@ public class IngredientTableModel extends AbstractTableModel implements EntityTa
         Ingredient removedIngredient = ingredients.get(rowIndex);
         List<Recipe> usedIn = new ArrayList<>();
         recipes.forEach(recipe -> {
-            if (recipe.getIngredients().stream().map(IngredientWithAmount::getIngredient).filter(ingredient -> ingredient.equals(removedIngredient)).toList().size() >
-                    0) {
+            if (recipe.getIngredients().stream().map(IngredientWithAmount::getIngredient)
+                      .filter(ingredient -> ingredient.equals(removedIngredient)).toList().size() > 0) {
                 usedIn.add(recipe);
             }
         });
 
         if (usedIn.size() > 0) {
             StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.append("Unable to delete Ingredient : ").append(removedIngredient.getName()).append("\nIt is used in Recipes: ");
+            stringBuilder.append("Unable to delete Ingredient : ").append(removedIngredient.getName())
+                         .append("\nIt is used in Recipes: ");
             for (Recipe recipe : usedIn) {
                 stringBuilder.append(" ").append(recipe.getName()).append(",");
             }
@@ -116,8 +106,10 @@ public class IngredientTableModel extends AbstractTableModel implements EntityTa
             JOptionPane.showMessageDialog(null, stringBuilder.toString(), "Error", ERROR_MESSAGE, null);
             return;
         }
-        ingredients.remove(rowIndex);
 
+        Ingredient ingredient = ingredients.get(rowIndex);
+        ingredientCrudService.deleteByGuid(ingredient.getGuid());
+        ingredients.remove(rowIndex);
         fireTableRowsDeleted(rowIndex, rowIndex);
     }
 }
