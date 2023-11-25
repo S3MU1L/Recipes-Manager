762da67dba164d6ac360f2915461a698a42753f9
xmalec <xmalec@fi.muni.cz>
2023-11-25 03:43:38 +0100

    implement DependencyProvider

--- src/main/java/cz/fi/muni/pv168/easyfood/wiring/DependencyProvider.java
+++ src/main/java/cz/fi/muni/pv168/easyfood/wiring/DependencyProvider.java
@@ -3,14 +3,10 @@ package cz.fi.muni.pv168.easyfood.wiring;
 
 import cz.fi.muni.pv168.easyfood.bussiness.model.Unit;
 import cz.fi.muni.pv168.easyfood.bussiness.repository.Repository;
-import cz.fi.muni.pv168.easyfood.bussiness.service.crud.CrudService;
 import cz.fi.muni.pv168.easyfood.bussiness.model.Category;
 import cz.fi.muni.pv168.easyfood.bussiness.model.Ingredient;
-import cz.fi.muni.pv168.easyfood.bussiness.model.IngredientWithAmount;
 import cz.fi.muni.pv168.easyfood.bussiness.model.Recipe;
 import cz.fi.muni.pv168.easyfood.storage.sql.db.DatabaseManager;
-import cz.fi.muni.pv168.easyfood.bussiness.service.export.ExportService;
-import cz.fi.muni.pv168.easyfood.bussiness.service.export.ImportService;
 
 /**
  * Interface for instance wiring
@@ -18,20 +14,6 @@ import cz.fi.muni.pv168.easyfood.bussiness.service.export.ImportService;
 public interface DependencyProvider {
 
     public DatabaseManager getDatabaseManager();
-
-    CrudService<Recipe> getRecipeCrudService();
-
-    CrudService<Ingredient> getIngredientCrudService();
-
-    CrudService<Category> getCategoryCrudService();
-
-    CrudService<IngredientWithAmount> getIngredientWithAmountCrudService();
-
-    CrudService<Unit> getUnitCrudService();
-
-    ImportService getImportService();
-
-    ExportService getExportService();
 
     Repository<Recipe> getRecipeRepository();
 
