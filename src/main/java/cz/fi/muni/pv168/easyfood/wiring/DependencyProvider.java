106a91ae10cb6c6eeb663a7d8d12b290cb8b51dd
xmalec <xmalec@fi.muni.cz>
2023-11-25 02:10:49 +0100

    add needed methods to interface

--- src/main/java/cz/fi/muni/pv168/easyfood/wiring/DependencyProvider.java
+++ src/main/java/cz/fi/muni/pv168/easyfood/wiring/DependencyProvider.java
@@ -4,13 +4,11 @@ package cz.fi.muni.pv168.easyfood.wiring;
 import cz.fi.muni.pv168.easyfood.bussiness.model.Unit;
 import cz.fi.muni.pv168.easyfood.bussiness.repository.Repository;
 import cz.fi.muni.pv168.easyfood.bussiness.service.crud.CrudService;
-import cz.fi.muni.pv168.easyfood.bussiness.service.validation.Validator;
 import cz.fi.muni.pv168.easyfood.bussiness.model.Category;
 import cz.fi.muni.pv168.easyfood.bussiness.model.Ingredient;
 import cz.fi.muni.pv168.easyfood.bussiness.model.IngredientWithAmount;
 import cz.fi.muni.pv168.easyfood.bussiness.model.Recipe;
 import cz.fi.muni.pv168.easyfood.storage.sql.db.DatabaseManager;
-import cz.fi.muni.pv168.easyfood.storage.sql.db.TransactionExecutor;
 import cz.fi.muni.pv168.easyfood.bussiness.service.export.ExportService;
 import cz.fi.muni.pv168.easyfood.bussiness.service.export.ImportService;
 
@@ -18,6 +16,8 @@ import cz.fi.muni.pv168.easyfood.bussiness.service.export.ImportService;
  * Interface for instance wiring
  */
 public interface DependencyProvider {
+
+    public DatabaseManager getDatabaseManager();
 
     CrudService<Recipe> getRecipeCrudService();
 
@@ -32,6 +32,14 @@ public interface DependencyProvider {
     ImportService getImportService();
 
     ExportService getExportService();
+
+    Repository<Recipe> getRecipeRepository();
+
+    Repository<Ingredient> getIngredientRepository();
+
+    Repository<Category> getCategoryRepository();
+
+    Repository<Unit> getUnitRepository();
 
 }
 
